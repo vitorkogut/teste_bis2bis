c3c645fce05588a24f9ff7076480229946308923
vitorkogut <vitorkogut@gmail.com>
2023-01-11 10:25:37 -0300

    Dia1

---
+++ controller_MongoDB.js
@@ -0,0 +1,61 @@
+const {MongoClient} = require('mongodb');
+const mongoose = require('mongoose');
+
+
+
+// FUNÇÃO DE INICIALIZAÇÃO DO MONGO
+async function start_mongo(){
+    try{
+        await mongoose.connect('mongodb://127.0.0.1:27017/teste_bis2bis');
+    }catch(e){
+        console.log("Um erro ocorreu na conexão com o Mongo " + e);
+    }
+    
+}
+
+
+// SCHEMAS E MODELS
+const faculdadeSchema = new mongoose.Schema({ // schema do modelo "faculdade"
+    web_pages : [String],
+    state_province : String,
+    alpha_two_code : String,
+    name : String,
+    country : String,
+    domains : [String]
+})
+const faculdade = mongoose.model("faculdade", faculdadeSchema); // model da faculdade
+
+
+// FUNÇÔES
+async function add_faculdade(nova_faculdade){
+    try{
+        const nova_faculdade_temp = await new faculdade(nova_faculdade);
+        await nova_faculdade_temp.save();
+        return true;
+    }catch(e){
+        console.log(e);
+        return false;
+    }
+}
+
+async function remove_all_faculdades(){
+    try{
+        await faculdade.deleteMany({});
+    }catch(e){
+        console.log(e);
+    }
+}
+
+async function get_faculdades(filtro, pagina_atual){
+    var result =[];
+    result = await faculdade.find(filtro, '_id name country',{skip: pagina_atual*20, limit:20});
+    return result;
+}
+
+
+module.exports = { 
+    start_mongo, 
+    add_faculdade,
+    remove_all_faculdades,
+    get_faculdades
+}
---
+++ index.js
@@ -0,0 +1,61 @@
+const express = require('express')
+const request = require('request')
+var mongoDB = require("./controller_MongoDB")
+const app = express()
+const port = 3000
+
+// inicialização do mongoDB
+mongoDB.start_mongo();
+
+
+// ROTAS DA API
+
+// rota para atualizar/obter dados das faculdades
+app.get('/getFaculdades', (req, res) => {
+    mongoDB.remove_all_faculdades(); // limpa a collection de dados antigos
+    const paises = ["argentina","brazil","chile","colombia","paraguai","peru","suriname","uruguay"]
+    const base_url = "http://universities.hipolabs.com/search?country="
+
+    paises.forEach(pais => { // para cada pais da lista
+        var lista_faculdades = [];
+
+        request(base_url+pais,{json:true}, (error, res, body) => { // obtenção das faculdades via JSON
+            if (error) {
+                return  console.log(error);
+            };
+            if (!error && res.statusCode == 200) { // caso não tenha erro na request
+
+                lista_faculdades = body; 
+                lista_faculdades.forEach(faculdade =>{ // adiciona cada uma das faculdades no Mongo
+                    var result = mongoDB.add_faculdade(faculdade);
+                    if(result == false){console.log("\nERRO AO INSERIR FACULDADE DO " + pais)}
+                });
+            };
+        });
+    });
+    res.send('Dados das faculdades obtidos!');
+})
+
+app.get('/universities', async(req, res) => {
+    const page_limit = 20;
+    const filtro = req.query;
+
+    if (filtro.page == null){ // caso não possua o numero da pagina vai para incial
+        filtro.page = 0;
+    }
+    const pagina_atual = filtro.page;
+    delete filtro.page;
+
+    if (filtro.country != null){
+        var resultado = await mongoDB.get_faculdades(filtro, pagina_atual);
+        res.send(resultado);
+    }else{
+        var resultado = await mongoDB.get_faculdades({}, pagina_atual);
+        res.send(resultado);
+    }
+    
+})
+
+app.listen(port, () => {
+  console.log(`Server de teste para Bis2Bis rodando na porta ${port}`)
+})
